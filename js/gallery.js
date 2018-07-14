7de3ad6a6626a75eec9f1bbc9dd1180cfd27f257
Christopher Restemayer <Restemayer@gmail.com>
2018-07-14 12:14:38 -0500

    Made the gallery functional.

--- js/gallery.js
+++ js/gallery.js
@@ -1,3 +1,24 @@
 function activateGallery() {
-  alert("Hello from the gallery file!");
+  let thumbnails = document.querySelectorAll("#gallery-thumbs > div > img");
+  let mainImage = document.querySelector("#gallery-photo img");
+  let currentClass = "current";
+  let galleryInfo = document.querySelector("#gallery-info");
+  let title = galleryInfo.querySelector(".title");
+  let description = galleryInfo.querySelector(".description");
+
+  thumbnails.forEach(function(thumbnail) {
+    largeVersion = new Image();
+    largeVersion.src = thumbnail.dataset.largeVersion;    
+
+    thumbnail.addEventListener("click", function () {
+      let newImageSrc = thumbnail.dataset.largeVersion;
+      mainImage.setAttribute("src", newImageSrc);
+
+      document.querySelector(".current").classList.remove(currentClass);
+
+      thumbnail.parentNode.classList.add(currentClass);
+      title.innerHTML = thumbnail.dataset.title;
+      description.innerHTML = thumbnail.dataset.description;
+    });
+  });
 }
